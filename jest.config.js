5a7cd8ead3505bb98361e9237fcde90dec8bbf09
Mathias Schaefer (molily) <molily@mailbox.org>
2023-01-25 17:08:48 +0100

    original PR

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ const config = {
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.svelte$': ['svelte-jester', { preprocess: true }],
   },
+  transformIgnorePatterns: [
+    // Transform some files in node_modules that are ESM-only
+    '/node_modules/(?!preact|preact-render-to-string)',
+  ],
   moduleFileExtensions: ['js', 'jsx', 'svelte'],
   testEnvironment: 'jsdom',
 };
